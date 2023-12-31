3586f6bcb276e86e066ef61c535efaf59bfd768d
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-31 10:01:12 +0200

    app-js-css

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Mitu" category="Mitu"></Person>
+      <Person name="Sahil" category="boy"></Person>
+      <Person name="Zarah" category="daughter"> </Person>
+      
     </div>
+  );
+}
+
+function Person (props){
+  console.log(props);
+  return ( 
+    <div className='person'>
+      <p>Name: {props.name}</p>
+      <p>Category: {props.category}</p>
+    </div>
+
   );
 }
 
